014be215168cc8d670305b9d3436d5c3d79dc8d0
emrekaragozz <34586398+emrekaragozz@users.noreply.github.com>
2020-06-14 21:24:26 +0300

    Syntax error is corrected.

--- HW9/src/hw9/Q20.java
+++ HW9/src/hw9/Q20.java
@@ -3,27 +3,27 @@ package hw9;
 public class Q20 {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("radical ");
-        sb = new StringBuilder("radical").append("robots");
 
-        System.out.println(sb);
+        sb = new StringBuilder("radical ")
+                .append("robots");
+        System.out.println(sb); //radical robots
 
         StringBuilder sb2 = new StringBuilder("radical ");
-        sb2 = new StringBuilder("radical ").delete(1, 100)
+        sb2 = new StringBuilder("radical ")
+                .delete(1, 100)
                 .append("obots")
                 .insert(1, "adical r");
-
-        System.out.println(sb2);
-
+        System.out.println(sb2); //radical robots
 
         StringBuilder sb3 = new StringBuilder("radical ");
         sb3 = new StringBuilder("radical ")
                 .insert(7, "robots");
-
-        System.out.println(sb3);
+        System.out.println(sb3); //radicalrobots
 
         StringBuilder sb4 = new StringBuilder("radical ");
-        sb4 = new StringBuilder("radical ").insert(sb4.length(), "robots");
-        System.out.println(sb4);
+        sb4 = new StringBuilder("radical ")
+                .insert(sb4.length(), "robots");
+        System.out.println(sb4); //radical robots
 
 
     }
